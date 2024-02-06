e430b600642ebc6f918680029661c8dbf81d5d89
Abhai Roop Gill <36809390+abhai28@users.noreply.github.com>
2024-02-05 23:59:22 -0500

    Initial commit

---
+++ src/main/java/abhai/projects/ems/EmsBackendApplication.java
@@ -0,0 +1,13 @@
+package abhai.projects.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsBackendApplication.class, args);
+	}
+
+}
