04df7fb11ce82d9fd41baf5e7b818117c8b75fec
Abhai Roop Gill <36809390+abhai28@users.noreply.github.com>
2024-02-06 14:26:47 -0500

    Implement EmployeeMapper for DTO and Entity Conversion
    - Added EmployeeMapper class in the package abhai.projects.ems.mapper.
    - This class provides two static methods:
       - `mapToEmployeeDto(Employee employee)`: Converts an Employee entity to an EmployeeDto.
       - `mapToEmployee(EmployeeDto employeeDto)`: Converts an EmployeeDto back to an Employee entity.
    
    These methods facilitate the conversion between the Employee entity and the EmployeeDto, ensuring a clean separation between the domain model and the data transfer objects used in the application.

---
+++ src/main/java/abhai/projects/ems/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package abhai.projects.ems.mapper;
+
+import abhai.projects.ems.dto.EmployeeDto;
+import abhai.projects.ems.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
