8dc5b4cf9a427d779c7ecd582bb2ad0c7015c844
Abhai Roop Gill <36809390+abhai28@users.noreply.github.com>
2024-02-06 14:57:48 -0500

    Add Create Employee API Endpoint
    - Implemented a new POST endpoint for creating employees in EmployeeController.
    - The endpoint '/api/employees' accepts an EmployeeDto object in the request body.
    - Utilizes the EmployeeService to persist the new employee and returns the saved EmployeeDto with a CREATED (201) HTTP status.
    
    This addition provides the client with the capability to add new employee records to the system via the REST API.

---
+++ src/main/java/abhai/projects/ems/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package abhai.projects.ems.controller;
+
+import abhai.projects.ems.dto.EmployeeDto;
+import abhai.projects.ems.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    // Build Add Employee REST API
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+}
