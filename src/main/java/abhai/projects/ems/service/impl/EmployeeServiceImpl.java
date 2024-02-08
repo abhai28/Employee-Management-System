0f286bf24e8afad54f6492d2485c7df69651fa05
Abhai Roop Gill <36809390+abhai28@users.noreply.github.com>
2024-02-08 10:57:38 -0500

    Add Update and Delete Employee API Endpoints
    - Enhanced EmployeeController with two new endpoints:
      - PUT '/api/employees/{id}': Updates an existing employee's details with the given ID.
      - DELETE '/api/employees/{id}': Deletes an employee with the specified ID.
    
    - Implemented updateEmployee and deleteEmployee methods in EmployeeServiceImpl:
      - The updateEmployee method fetches an employee by ID and applies updated information before saving.
      - The deleteEmployee method ensures the employee exists before proceeding with deletion.
    
    - Extended EmployeeService interface to include updateEmployee and deleteEmployee method signatures.
    
    These changes allow for full CRUD operations on employee records via the REST API, promoting better data management and control.

--- src/main/java/abhai/projects/ems/service/impl/EmployeeServiceImpl.java
+++ src/main/java/abhai/projects/ems/service/impl/EmployeeServiceImpl.java
@@ -40,5 +40,28 @@ public class EmployeeServiceImpl implements EmployeeService {
                 .collect(Collectors.toList());
     }
 
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(()->
+                        new ResourceNotFoundException("Employee does not exist with give id: "+employeeId));
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+
+        Employee updatedEmployeeObj = employeeRepository.save(employee);
+
+        return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
+    }
+
+    @Override
+    public void deleteEmployee(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(()->
+                        new ResourceNotFoundException("Employee does not exist with give id: "+employeeId));
+
+        employeeRepository.deleteById(employeeId);
+    }
+
 
 }
