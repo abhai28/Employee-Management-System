ba955dc04e3739e0dfd2c81c3ae65472f4612cbf
Abhai Roop Gill <36809390+abhai28@users.noreply.github.com>
2024-02-12 22:36:23 -0500

    Added Route for /employees

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -2,12 +2,19 @@ import ListEmployee from "./components/ListEmployee";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <Header />
-      <ListEmployee />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ListEmployee />}></Route>
+          <Route path="/employees" element={<ListEmployee />}></Route>
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
